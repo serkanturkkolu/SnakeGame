20c66678a2c4269ce21461cef30fc095db12d3cf
Serkan TURKKOLU <serkanturkkolu@gmail.com>
2021-05-01 18:16:39 +0300

    Add files via upload

---
+++ build/CMakeFiles/SnakeGame.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/serkan/SnakeGame/dot.cpp" "/home/serkan/SnakeGame/build/CMakeFiles/SnakeGame.dir/dot.cpp.o"
+  "/home/serkan/SnakeGame/main.cpp" "/home/serkan/SnakeGame/build/CMakeFiles/SnakeGame.dir/main.cpp.o"
+  "/home/serkan/SnakeGame/snake.cpp" "/home/serkan/SnakeGame/build/CMakeFiles/SnakeGame.dir/snake.cpp.o"
+  "/home/serkan/SnakeGame/snakegame.cpp" "/home/serkan/SnakeGame/build/CMakeFiles/SnakeGame.dir/snakegame.cpp.o"
+  "/home/serkan/SnakeGame/snakemap.cpp" "/home/serkan/SnakeGame/build/CMakeFiles/SnakeGame.dir/snakemap.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
